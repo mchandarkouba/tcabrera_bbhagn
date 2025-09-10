d01e12daeec078697226d7671f363d2c56c08afa
tomas-cabrera <tcabrera@andrew.cmu.edu>
2025-09-10 11:24:59 -0400

    Refactoring; implemented simulations, still some issues with running analysis on sims

--- utils/simulate.py
+++ utils/simulate.py
@@ -1,7 +1,6 @@
 ### This module handles the simulation of a follow-up suite.
 
 ### Inputs:
-#     - df_fitparams (flarename, filter, t_peak, f_peak, t_rise, t_decay, f_base)
 #     - DF_GW (GWname --> skymap, f_cover)
 #     - sim config (lambda, H0, Om0, AGN distribution, flare model)
 
@@ -24,6 +23,8 @@
 # Do a version where only massive events produce flares as well, and see if it is distinguishable from the other version
 
 import multiprocessing as mp
+import os.path as pa
+import sys
 
 import astropy.units as u
 import astropy_healpix as ah
@@ -32,10 +33,17 @@ import ligo.skymap.moc as lsm_moc
 import numpy as np
 import pandas as pd
 import parmap
+import yaml
 from astropy.coordinates import SkyCoord
-from astropy.cosmology import z_at_value
+from astropy.cosmology import FlatLambdaCDM, z_at_value
+from astropy.io import fits
+from astropy.time import Time
 from ligo.skymap.io import read_sky_map
 from ligo.skymap.postprocess.crossmatch import crossmatch
+
+PROJDIR = pa.dirname(pa.dirname(__file__))
+sys.path.append(PROJDIR)
+from myagn import distributions as myagndistributions
 
 
 def draw_continuous_from_discrete(
@@ -44,16 +52,17 @@ def draw_continuous_from_discrete(
     rng=np.random.default_rng(12345),
     n_draws=1,
 ):
-    """Draws continuous values from a discrete probability distribution via Von Neumann.
-    Uses linear interpolation to determine probability at arbitrary points.
-    """
-    draws = []
-    max_p = np.max(x_pdf) * 1.1
-    while len(draws) < n_draws:
-        a = rng.uniform(np.min(x_grid), np.max(x_grid))
-        b = rng.uniform(0, max_p)
-        if b <= np.interp(a, x_grid, x_pdf):
-            draws.append(a)
+    """Chooses a bin by trapezoid probability per bin, then draws from linear distribution within bin."""
+    # Calculate probabilities per bin
+    bin_probs = 0.5 * (x_pdf[:-1] + x_pdf[1:]) * (x_grid[1:] - x_grid[:-1])
+    bin_probs /= np.sum(bin_probs)
+    # Choose bins
+    idxs = rng.choice(np.arange(len(bin_probs)), p=bin_probs, size=n_draws)
+    # Draw uniformly from bin (tried with trapezoidal area, but a bit complicated)
+    fracs = rng.uniform(0, 1, n_draws)
+    x_los = x_grid[idxs]
+    x_his = x_grid[idxs + 1]
+    draws = x_los + fracs * (x_his - x_los)
     if n_draws == 1:
         return draws[0]
     else:
@@ -91,6 +100,7 @@ def _draw_gw_flare_coords(
     gw_row,
     dl_grid,
     dt_followup,
+    cosmo,
 ):
     """Draws flare coordinates from GW skymap.
     Uses the df_gw_input and sc_flares variables defined above.
@@ -137,30 +147,30 @@ def _draw_gw_flare_coords(
     dec = dec.to(u.deg).value
     # Draw redshift
     dl_pdf = lsm_dist.conditional_pdf(
-        dl_grid,
+        dl_grid.to(u.Mpc).value,
         [distmu] * len(dl_grid),
         [distsigma] * len(dl_grid),
         [distnorm] * len(dl_grid),
     )
     dl = draw_continuous_from_discrete(
-        dl_grid,
+        dl_grid.to(u.Mpc).value,
         dl_pdf,
         rng=rng,
     )
-    z = z_at_value(cosmo.luminosity_distance, dl)
+    z = z_at_value(cosmo.luminosity_distance, dl * u.Mpc)
     # Return
     return {
-        "t": t,
+        "mjd": t,
         "ra": ra,
         "dec": dec,
-        "z": z,
+        "Redshift": z.value,
     }
 
 
 def simulate_flares(
     lamb,
     cosmo,
-    df_gw_input,  # Should include skymap_path, t, f_cover
+    df_gw_path,  # Should include skymap_path, t, f_cover
     agn_dist,
     dt_followup,
     z_grid=np.linspace(0, 2, 1000),
@@ -170,6 +180,15 @@ def simulate_flares(
     # Setup
     dl_grid = cosmo.luminosity_distance(z_grid)
     dcm_grid = cosmo.comoving_distance(z_grid)
+    df_gw_input = pd.read_csv(df_gw_path)
+
+    # Get GW times
+    ts = []
+    for _, row in df_gw_input.iterrows():
+        with fits.open(row["skymap_path"]) as hdul:
+            t = Time(hdul[1].header["MJD-OBS"], format="mjd").mjd
+            ts.append(t)
+    df_gw_input["t"] = ts
 
     print("Generating background flares...")
     # Background flares
@@ -180,15 +199,16 @@ def simulate_flares(
     n_flares_bg = rng.poisson(n_flares_bg_avg)
     print(f"Average number of background flares: {n_flares_bg_avg}")
     # Draw times
+    t_min_bg = np.nanmin(df_gw_input["t"])
     t_max_bg = np.nanmax(df_gw_input["t"])
-    t_flares_bg = rng.uniform(0, t_max_bg + dt_followup, n_flares_bg) * u.day
+    t_flares_bg = rng.uniform(t_min_bg, t_max_bg + dt_followup, n_flares_bg) * u.day
     # Draw RAs, decs
     ra_flares_bg = rng.uniform(0, 360, n_flares_bg)
     dec_flares_bg = 180 / np.pi * np.arcsin(rng.uniform(-1, 1, n_flares_bg))
     # Draw redshifts
     z_flares_bg = draw_continuous_from_discrete(
         z_grid,
-        dn_dz,
+        dn_dz.value,
         rng=rng,
         n_draws=n_flares_bg,
     )
@@ -213,10 +233,10 @@ def simulate_flares(
     cm_mask = np.any(cm_masks, axis=0)
     df_flares_bg = pd.DataFrame(
         {
-            "t": t_flares_bg,
+            "mjd": t_flares_bg,
             "ra": ra_flares_bg,
             "dec": dec_flares_bg,
-            "z": z_flares_bg,
+            "Redshift": z_flares_bg,
         }
     )[cm_mask]
     df_flares_bg["gw"] = False
@@ -244,6 +264,7 @@ def simulate_flares(
         gw_rows,
         dl_grid,
         dt_followup,
+        cosmo,
         pm_processes=nproc,
     )
     df_flares_gw = pd.DataFrame(gw_flare_coords)
@@ -251,35 +272,65 @@ def simulate_flares(
 
     # Return
     df_flares = pd.concat([df_flares_bg, df_flares_gw], ignore_index=True)
+    df_flares["flarename"] = np.arange(df_flares.shape[0])
     return df_flares
 
 
-if __name__ == "__main__":
-    from astropy.cosmology import FlatLambdaCDM
-    from myagn import distributions as myagndistributions
+def main(config_path):
+    config = yaml.safe_load(open(config_file))
+
+    # Parse AGN distribution config
+    for k, v in config["agn_distribution"].items():
+        if v["model"] == "ConstantPhysicalDensity":
+            v["args"] = v["args"] * u.Mpc**-3
+        if "brightness_limits" in v["density_kwargs"]:
+            if "brightness_units" not in v["density_kwargs"]:
+                raise ValueError(
+                    "Must specify brightness_units if brightness_limits is given."
+                )
+            if v["density_kwargs"]["brightness_units"] == "ABmag":
+                bu = u.ABmag
+            elif v["density_kwargs"]["brightness_units"] == "erg/s":
+                bu = u.erg / u.s
+            else:
+                raise ValueError("brightness_units must be 'ABmag' or 'erg/s'.")
+            v["density_kwargs"]["brightness_limits"] = [
+                float(bl) for bl in v["density_kwargs"]["brightness_limits"]
+            ] * bu
+            v["density_kwargs"].pop("brightness_units")
 
     # Setup
-    lamb = 0.3
-    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-    df_gw_input = pd.read_csv("data/df_gw_input.csv")
+    cosmo = FlatLambdaCDM(H0=config["H0"], Om0=config["Om0"])
+    agndist_config = config["agn_distribution"]
     agn_dist = getattr(
         myagndistributions,
-        # agndist_config["model"],
-        "ConstantPhysicalDensity",
+        agndist_config["observed"]["model"],
     )(
-        # *agndist_config["args"],
-        # **agndist_config["kwargs"],
-        10**-4.75,
-        {},
+        *agndist_config["observed"]["args"],
+        **agndist_config["observed"]["kwargs"],
     )
-    dt_followup = 200
 
     # Simulate
     df_flares = simulate_flares(
-        lamb,
+        config["lambda"],
         cosmo,
-        df_gw_input,
+        config["gw_csv"],
         agn_dist,
-        dt_followup,
+        config["dt_followup"],
     )
-    df_flares.to_csv("data/df_flares.csv", index=False)
+
+    # Save
+    df_flares.to_csv(pa.join(pa.dirname(config_path), "flares.csv"), index=False)
+
+    # Done
+    print("Done.")
+    return
+
+
+if __name__ == "__main__":
+
+    # Load yaml file
+    config_file = sys.argv[
+        1
+    ]  # "/hildafs/home/tcabrera/HIPAL/bbhagn/bbhagn/config.yaml"
+    main(config_file)
